f804c8c824d6227ce3e2de2d50449ffa3117fa0b
Elouan <elouanfou@gmail.com>
2025-12-05 09:48:25 +0100

    fichier config

--- config/database.php
+++ config/database.php
@@ -1,1 +1,12 @@
 <?php
+$host = 'localhost';
+$dbname = 'immoform1';
+$username = 'root';
+$password = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Erreur de connexion : " . $e->getMessage());
+}
