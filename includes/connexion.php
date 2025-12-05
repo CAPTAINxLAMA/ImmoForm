b7077bbce8a88c2ea2828dbf22d33bc7f9876d42
Elouan <elouanfou@gmail.com>
2025-12-05 11:23:53 +0100

    inscription.php et connexion.php maj

--- includes/connexion.php
+++ includes/connexion.php
@@ -1,1 +1,21 @@
-<?php
+<?php include 'header.php'?>
+
+<main>
+    <div class="container">
+        <form method="POST">
+            <label>Email :</label>
+            <input type="email" name="email" required>
+
+            <label>Mot de passe :</label>
+            <input type="password" name="password" required minlength="8">
+
+            <button type="submit">Se connecter</button>
+        </form>
+
+        <p style="margin-top: 20px;">
+            Pas encore de compte ? <a href="inscription.php">Créer un compte</a>
+        </p>
+    </div>
+</main>
+
+<?php include 'footer.php'?>
