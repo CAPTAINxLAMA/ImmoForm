25dd7e0fc0c606e737606871f3a181bccfe59dfc
Elouan <elouanfou@gmail.com>
2025-12-15 09:37:37 +0100

    Page gérer les demandes admin terminé, création des fichiers accpeter demande et refuser demande

--- admin/gerer_demandes.php
+++ admin/gerer_demandes.php
@@ -9,38 +9,34 @@
     //Php Data Object → fait le lien avec une base de donnée prédéfinie
     $pdo = new PDO("mysql:host=".config::host.";dbname=".config::dbname, config::user, config::password);
 
-    $req = $pdo->prepare("select * from conseil");
+    $req = $pdo->prepare("select * from demandeconseil");
     $req->execute(); //exécute le select /\
     $conseils = $req->fetchAll(); //va interpréter chaque ligne de la base de donnée en tant que dictionnaire php
     ?>
 
     <table class="table table-stripped">
         <tr>
-            <th>Titre | </th>
+            <th>Type | </th>
             <th>Description | </th>
-            <th>Durée | </th>
             <th>Date | </th>
-            <th>Lieu | </th>
-            <th>Support | </th>
-            <th>Commentaire</th>
+            <th>Statut</th>
+            <th>Gérer par</th>
         </tr>
         <?php
         foreach ($conseils  as $conseil)
         {
             ?>
             <tr>
-                <td><?php echo $conseil["Titre"] ?></td>
+                <td><?php echo $conseil["Type"] ?></td>
                 <td><?php echo $conseil["Description"] ?></td>
-                <td><?php echo $conseil["Duree"] ?></td>
                 <td><?php echo $conseil["Date"] ?></td>
-                <td><?php echo $conseil["Lieu"] ?></td>
-                <td><?php echo $conseil["Support"] ?></td>
-                <td><?php echo $conseil["Commentaire"] ?></td>
+                <td><?php echo $conseil["Statut"] ?></td
+                <td><?php echo $conseil["Formateur_id"] ?></td>
                 <td>
-                    <a href="/actions/modifierDemande.php?id=<?php echo $conseil["id"] ?>"
-                       class="btn btn-sm btn-warning">Examiner</a>
-                    <a href="/actions/supprimerDemande.php?id=<?php echo $conseil["id"] ?>"
-                       class="btn btn-sm btn-danger">Supprimer</a>
+                    <a href="/actions/accepterDemande.php?id=<?php echo $conseil["id"] ?>"
+                       class="btn btn-sm btn-warning">Prendre en charge</a>
+                    <a href="/actions/refuserDemande.php?id=<?php echo $conseil["id"] ?>"
+                       class="btn btn-sm btn-danger">Refuser</a>
                 </td>
             </tr>
             <?php
