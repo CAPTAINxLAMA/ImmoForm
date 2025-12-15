4a97db8c1e6168b3cc27ee2674c486d98c889c2e
LENOVOI5 <ndougnamallan@gmail.com>
2025-12-15 09:12:50 +0100

    le champs d

--- client/nouvelle_demande.php
+++ client/nouvelle_demande.php
@@ -6,16 +6,17 @@
     <div class="container">
         <form method="POST">
             <label>Type de demande :</label>
-            <input type="text" name="text" required>
+            <input type="text" name="type_demande" required>
 
             <label>Type de conseil :</label>
-            <input type="text" name="text" required>
+            <input type="text" name="type_conseil" required>
 
             <label>Description détaillée de la demande :</label>
-            <input type="text" name="text" required>
+            <input type="text" name="description" required>
 
             <label>Date de la demande :</label>
-            <input type="text" name="readonly" readonly>
+            <input type="date" name="date_demande" value="<?php echo date('Y-m-d'); ?>" readonly>
+
 
             <label>Contact de l'agence :</label>
             <input type="text" name="text">
