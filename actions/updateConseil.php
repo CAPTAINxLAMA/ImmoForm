b870336afc14a1d4e0d1eaa88e71c0bc60695dca
LENOVOI5 <ndougnamallan@gmail.com>
2025-12-17 16:01:01 +0100

    modifierConseil.php et updateConseil.php mais ils ne sont pas encore fonctionnel

--- actions/updateConseil.php
+++ actions/updateConseil.php
@@ -1,1 +1,42 @@
+
 <?php
+session_start();
+$tokenServeur= $_SESSION['token'];
+$tokenRecu=filter_input(INPUT_POST, 'token', FILTER_DEFAULT);
+
+//je vérifie la cohérence des tokens
+if($tokenRecu != $tokenServeur){
+    die("Erreur de token. Va mourir vilain hacker.");//je stoppe tout
+}
+
+//on récupère les données du POST
+$Titre = filter_input(INPUT_POST, 'titre', FILTER_DEFAULT);
+$Description = filter_input(INPUT_POST, 'description', FILTER_DEFAULT);
+$Duree = filter_input(INPUT_POST, 'duree', FILTER_DEFAULT);
+$Date = date("Y-m-d-H-m-s");
+$Cout = filter_input(INPUT_POST, 'cout', FILTER_DEFAULT);
+$Commentaire = filter_input(INPUT_POST, 'commentaire', FILTER_DEFAULT);
+$Support = filter_input(INPUT_POST, 'support', FILTER_DEFAULT);
+$Lieu = filter_input(INPUT_POST, 'lieu', FILTER_DEFAULT);
+$id=filter_input(INPUT_POST, 'id', FILTER_VALIDATE_INT);
+
+include "../includes/config.php";
+$pdo = new PDO("mysql:host=".config::host.";dbname=".config::dbname, config::user, config::password);
+$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+//on prépare la requête avec des bindParam pour éviter les injections SQL
+$req=$pdo->prepare("INSERT INTO conseil (Titre, Description, Duree, Date, Cout, Commentaire, Support, Lieu, id) VALUES (:Titre, :Description, :Duree, :Date, :Cout, :Commentaire, :Support, :Lieu, :id)");
+$req->bindParam(':Titre', $Titre);
+$req->bindParam(':Description', $Description);
+$req->bindParam(':Duree', $Duree);
+$req->bindParam(':Date', $Date);
+$req->bindParam(':Cout', $Cout);
+$req->bindParam(':Commentaire', $Commentaire);
+$req->bindParam(':Support', $Support);
+$req->bindParam(':Lieu', $Lieu);
+$req->bindParam(':id', $id);
+$req->execute();
+
+//retour à la page d'accueil
+header("Location: ../admin/navbar.php");
+
--- admin/modifierConseil.php
+++ admin/modifierConseil.php
@@ -1,1 +1,84 @@
 <?php
+include('../includes/header.php');
+
+// vérification des autorisations
+require '../includes/auth.php';
+
+requireRole('admin');
+
+include_once('../includes/config.php');
+
+$token = rand(0, 1000000);
+$_SESSION['token'] = $token;
+
+$id = filter_input(INPUT_GET, "id", FILTER_VALIDATE_INT);
+
+if (!$id) {
+    http_response_code(404);
+    die("ID manquant ou invalide");
+}
+
+// Connexion à la base de données
+$pdo = new PDO("mysql:host=".config::host.";dbname=".config::dbname, config::user, config::password);
+
+$req=$pdo->prepare("SELECT * FROM conseil WHERE Id=:id");
+$req->bindParam(':id', $id);
+$req->execute();
+
+$conseil=$req->fetchAll();
+
+// vérification que j'en ai bien récupéré une seule
+if(count($conseil)!=1){
+    http_response_code(404);
+    die("Pas de conseil pour l'id ".$id);
+}
+
+?>
+
+<div class="container">
+    <h2>Modifier le conseil :</h2>
+    <br>
+    <form action="../actions/updateConseil.php" method="post">
+
+        <main>
+            <div class="container">
+                <h2>Un conseil :</h2>
+                <br>
+                <form method="POST" action="../actions/createConseil.php">
+                    <label>Titre :</label>
+                    <input type="text" name="titre" maxlength="50" required value="<?php echo htmlentities($conseil[0]["Titre"]) ?>">
+
+                    <label>Description détaillée :</label>
+                    <input type="text" name="description" required value="<?php echo htmlentities($conseil[0]["Description"]) ?>">
+
+                    <label>Durée :</label>
+                    <input type="number" name="duree" placeholder="En heure" required value="<?php echo htmlentities($conseil[0]["Duree"]) ?>">
+
+                    <!--récupération de la date du jour-->
+
+                    <label>Coût :</label>
+                    <input type="number" step="0.01" name="cout" placeholder="En euro" required value="<?php echo htmlentities($conseil[0]["Cout"]) ?>">
+
+                    <label>Commentaire :</label>
+                    <input type="text" name="commentaire" placeholder="Factultatif" value="<?php echo htmlentities($conseil[0]["Commentaire"]) ?>">
+
+                    <label>Support :</label>
+                    <input type="text" name="support" placeholder="Factultatif" value="<?php echo htmlentities($conseil[0]["Support"]) ?>">
+
+                    <label>Lieu :</label>
+                    <input type="text" name="lieu" required value="<?php echo htmlentities($conseil[0]["Lieu"]) ?>">
+
+                    <input type="hidden" name="id" value="<?php echo $id ?>"/>
+                    <input type="hidden" name="token" value="<?php echo $token; ?>">
+
+                    <button type="submit" class="btn3">Enregistrer</button>
+                    <a href="mes_demandes.php" class="btn">Annuler</a>
+                </form>
+
+            </div>
+        </main>
+    </form>
+</div>
+
+<?php include "../includes/footer.php"; ?>
+
