fd1565f1126eeb35a37325e0df0817d4bd9fd6e6
Dev <haspotfrancis@gmail.com>
2025-12-18 09:34:35 +0100

    demande validée (client)

--- client/demande_validee.php
+++ client/demande_validee.php
@@ -1,22 +1,26 @@
-<?php include '../includes/header.php';
+<?php
+
+include '../includes/header.php';
 
 require '../includes/auth.php';
 
 requireRole('client');
 
+$token = rand(0, 1000000);
+$_SESSION['token'] = $token;
+
 include_once "../includes/config.php";
 
-$Id=filter_input(INPUT_GET, 'Id', FILTER_VALIDATE_INT);
+$Id=filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
 
 $pdo = new PDO("mysql:host=" . config::host . ";dbname=" . config::dbname, config::user, config::password);
 
-$req = $pdo->prepare("SELECT * FROM conseil WHERE Demande_Id = :Id ");
+$req = $pdo->prepare("SELECT * FROM conseil LEFT JOIN formateur ON conseil.Formateur_ID = formateur.Id WHERE Demande_Id = :Id");
 $req->bindParam(':Id', $Id);
 
 $req->execute();
 
-$conseils = $req->fetchAll();
-
+$demandes = $req->fetchAll();
 ?>
 
 <div class="container">
@@ -24,7 +28,7 @@ $conseils = $req->fetchAll();
 
     <table>
         <tr>
-            <th>Type </th>
+            <th>Titre </th>
             <th>Description </th>
             <th>Durée</th>
             <th>Date </th>
@@ -32,20 +36,22 @@ $conseils = $req->fetchAll();
             <th>Support </th>
             <th>Coût</th>
             <th>Commentaire</th>
+            <th>Formateur</th>
         </tr>
         <?php
-        foreach ($conseils  as $conseil)
+        foreach ($demandes  as $demande)
         {
             ?>
             <tr>
-                <td><?php echo $conseil["Type"] ?></td>
-                <td><?php echo $conseil["Description"] ?></td>
-                <td><?php echo $conseil["Duree"] ?> minutes</td>
-                <td><?php echo $conseil["Date"] ?></td>
-                <td><?php echo $conseil["Lieu"] ?></td>
-                <td><?php echo $conseil["Support"] ?></td>
-                <td><?php echo $conseil["Cout"] ?> €</td>
-                <td><?php echo $conseil["Commentaire"] ?></td>
+                <td><?php echo $demande["Titre"] ?></td>
+                <td><?php echo $demande["Description"] ?></td>
+                <td><?php echo $demande["Duree"] ?> minutes</td>
+                <td><?php echo $demande["Date"] ?></td>
+                <td><?php echo $demande["Lieu"] ?></td>
+                <td><?php echo $demande["Support"] ?></td>
+                <td><?php echo $demande["Cout"] ?> €</td>
+                <td><?php echo $demande["Commentaire"] ?></td>
+                <td><?php echo $demande["Nom"]." ".$demande["Prenom"] ?></td>
             </tr>
             <?php
         }
